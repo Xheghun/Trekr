2404ac68c20c8bf7f912ae4dca88bdd535673187
Xheghun <ysenruf@gmail.com>
2023-01-23 14:19:28 +0100

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by mycover Mobile on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
